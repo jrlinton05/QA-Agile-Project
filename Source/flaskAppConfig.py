8cd8eda8ad1c99e4d8639a342ec70ccf6ac144bd
Jack Linton <jrlinton@amazon.com>
2025-06-17 17:23:59 +0100

    Add first HTML template as test and render with Flask

---
+++ Source/flaskAppConfig.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# Routes the main page of the site to the relevant HTML template
+@app.route("/")
+def run_test_page():
+    return render_template('testPage.html')
+
+# If this file is ran from the IDE, deploy the website locally in debug mode
+if __name__ == "__main__":
+    app.run(debug=True)
