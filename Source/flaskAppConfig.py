1f5349824705e7a9ba86e9c89f794c8ea09f024d
Jack Linton <jrlinton@amazon.com>
2025-06-17 21:44:10 +0100

    Add registration page framework and routing

--- Source/flaskAppConfig.py
+++ Source/flaskAppConfig.py
@@ -2,10 +2,22 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# Routes the main page of the site to the relevant HTML template
+
+# Flask app page routing
 @app.route("/")
-def run_test_page():
+def get_root_page():
     return render_template('loginPage.html')
+
+
+@app.route("/login")
+def get_login_page():
+    return render_template('loginPage.html')
+
+
+@app.route("/register")
+def get_register_page():
+    return render_template('registerPage.html')
+
 
 # If this file is ran from the IDE, deploy the website locally in debug mode
 if __name__ == "__main__":
